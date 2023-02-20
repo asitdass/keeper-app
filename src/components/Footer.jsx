e703e237c3b5e1f5a0e9bc15a70bda98baedc29b
Asit Rohan Dass <asitdass33@gmail.com>
2023-02-21 01:17:53 +0530

    Kee[er start

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Footer(){
+    return(
+        <div className="footer">
+        <p>Copyright @ 2023</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Note(){
+    return(
+        <div className="note">
+        <p>This is the note title</p>
+        <p>This is the note Content</p>
+        </div>
+    );
+}
+
+export default Note;
